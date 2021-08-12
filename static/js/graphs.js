c430e71d576781e981f808b38f6a3435e0f7af24
cschmi14 <cschmidt542@gmail.com>
2021-08-12 02:25:22 -0500

    modify css

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -865,7 +865,350 @@ d3.json("/asthma/projects").then(function(data) {
             }
 
             window.addEventListener('resize', function() {
-                usChart.width(null)
+                totalCasesND
+        .formatNumber(d3.format(",d"))
+        .valueAccessor(function(d){return d; })
+        .html({
+            some:"<span style=\"color:black; font-size: 60px;\">%number</span>",
+          })
+        .group(avgCases).valueAccessor(function(d) {
+            var maxYear;
+            var minYear;
+            if (avgCases.value().cases2023 > 1) {
+                maxYear = 2023;
+            }
+            else if (avgCases.value().cases2022 > 1) {
+                maxYear = 2022;
+            }
+            else if (avgCases.value().cases2021 > 1) {
+                maxYear = 2021;
+            }
+            else if (avgCases.value().cases2020 > 1) {
+                maxYear = 2020;
+            }
+            else if (avgCases.value().cases2019 > 1) {
+                maxYear = 2019;
+            }
+            else if (avgCases.value().cases2018 > 1) {
+                maxYear = 2018;
+            }
+            else if (avgCases.value().cases2017 > 1) {
+                maxYear = 2017;
+            }
+            else if (avgCases.value().cases2016 > 1) {
+                maxYear = 2016;
+            }
+            else if (avgCases.value().cases2015 > 1) {
+                maxYear = 2015;
+            }
+            else if (avgCases.value().cases2014 > 1) {
+                maxYear = 2014;
+            }
+            else if (avgCases.value().cases2013 > 1) {
+                maxYear = 2013;
+            }
+            else if (avgCases.value().cases2012 > 1) {
+                maxYear = 2012;
+            }
+            else if (avgCases.value().cases2011 > 1) {
+                maxYear = 2011;
+            }
+
+            if (avgCases.value().cases2011 > 0) {
+                minYear = 2011;
+            }
+            else if (avgCases.value().cases2012 > 0) {
+                minYear = 2012;
+            }
+            else if (avgCases.value().cases2013 > 0) {
+                minYear = 2013;
+            }
+            else if (avgCases.value().cases2014 > 0) {
+                minYear = 2014;
+            }
+            else if (avgCases.value().cases2015 > 0) {
+                minYear = 2015;
+            }
+            else if (avgCases.value().cases2016 > 0) {
+                minYear = 2016;
+            }
+            else if (avgCases.value().cases2017 > 0) {
+                minYear = 2017;
+            }
+            else if (avgCases.value().cases2018 > 0) {
+                minYear = 2018;
+            }
+            else if (avgCases.value().cases2019 > 0) {
+                minYear = 2019;
+            }
+            else if (avgCases.value().cases2020 > 0) {
+                minYear = 2020;
+            }
+            else if (avgCases.value().cases2021 > 0) {
+                minYear = 2021;
+            }
+            else if (avgCases.value().cases2022 > 0) {
+                minYear = 2022;
+            }
+            else if (avgCases.value().cases2023 > 0) {
+                minYear = 2023;
+            }
+
+            return (sumCases.value() / ((maxYear - minYear) * 4));
+        });
+
+        avgCasesND
+        .formatNumber(d3.format("~r"))
+        .html({
+            some:"<span style=\"color:black; font-size: 60px;\">%number</span>",
+          })
+        .group(avgPercent);
+
+        avgCasesND.value = function() {
+            return (Math.round(avgPercent.value().avg * 1000) / 1000);
+        };
+
+        avgPercentND
+        .formatNumber(d3.format(",d"))
+        .html({
+            some:"<span style=\"color:black; font-size: 60px;\">%number</span>",
+          })
+        .group(avgCases).valueAccessor(function() {
+            var maxYear;
+            var minYear;
+            if (avgCases.value().cases2023 > 1) {
+                maxYear = avgCases.value().cases2023;
+            }
+            else if (avgCases.value().cases2022 > 1) {
+                maxYear = avgCases.value().cases2022;
+            }
+            else if (avgCases.value().cases2021 > 1) {
+                maxYear = avgCases.value().cases2021;
+            }
+            else if (avgCases.value().cases2020 > 1) {
+                maxYear = avgCases.value().cases2020;
+            }
+            else if (avgCases.value().cases2019 > 1) {
+                maxYear = avgCases.value().cases2019;
+            }
+            else if (avgCases.value().cases2018 > 1) {
+                maxYear = avgCases.value().cases2018;
+            }
+            else if (avgCases.value().cases2017 > 1) {
+                maxYear = avgCases.value().cases2017;
+            }
+            else if (avgCases.value().cases2016 > 1) {
+                maxYear = avgCases.value().cases2016;
+            }
+            else if (avgCases.value().cases2015 > 1) {
+                maxYear = avgCases.value().cases2015;
+            }
+            else if (avgCases.value().cases2014 > 1) {
+                maxYear = avgCases.value().cases2014;
+            }
+            else if (avgCases.value().cases2013 > 1) {
+                maxYear = avgCases.value().cases2013;
+            }
+            else if (avgCases.value().cases2012 > 1) {
+                maxYear = avgCases.value().cases2012;
+            }
+            else if (avgCases.value().cases2011 > 1) {
+                maxYear = avgCases.value().cases2011;
+            }
+
+            if (avgCases.value().cases2011 > 0) {
+                minYear = avgCases.value().cases2011;
+            }
+            else if (avgCases.value().cases2012 > 0) {
+                minYear = avgCases.value().cases2012;
+            }
+            else if (avgCases.value().cases2013 > 0) {
+                minYear = avgCases.value().cases2013;
+            }
+            else if (avgCases.value().cases2014 > 0) {
+                minYear = avgCases.value().cases2014;
+            }
+            else if (avgCases.value().cases2015 > 0) {
+                minYear = avgCases.value().cases2015;
+            }
+            else if (avgCases.value().cases2016 > 0) {
+                minYear = avgCases.value().cases2016;
+            }
+            else if (avgCases.value().cases2017 > 0) {
+                minYear = avgCases.value().cases2017;
+            }
+            else if (avgCases.value().cases2018 > 0) {
+                minYear = avgCases.value().cases2018;
+            }
+            else if (avgCases.value().cases2019 > 0) {
+                minYear = avgCases.value().cases2019;
+            }
+            else if (avgCases.value().cases2020 > 0) {
+                minYear = avgCases.value().cases2020;
+            }
+            else if (avgCases.value().cases2021 > 0) {
+                minYear = avgCases.value().cases2021;
+            }
+            else if (avgCases.value().cases2022 > 0) {
+                minYear = avgCases.value().cases2022;
+            }
+            else if (avgCases.value().cases2023 > 0) {
+                minYear = avgCases.value().cases2023;
+            }
+
+            return ((maxYear - minYear) / 4);
+
+        }
+
+        );
+
+        ageCasesChart
+        .width(null)
+        .height(usChart.height() + usChart.width() / 100)
+        .ordering(function(d){ return -d.value.avg})
+        .margins({ top: 10, left: 40, right: 40, bottom: 75})
+        .dimension(ageDim)
+        .group(percentByAge).valueAccessor(function(d) {
+            if (d.key != 1)
+            return Math.round(d.value.avg * 1000) / 1000;
+        })
+        .x(d3.scaleOrdinal().domain(["18-24", "55-64", "45-54", "35-44", "25-34", "65+"]))
+        .xUnits(dc.units.ordinal)
+        .yAxisLabel("Average Asthma Percentage")
+        .xAxisLabel("Age")
+        .elasticY(true)
+        .transitionDuration(0)
+        .gap(10)
+        .renderlet(
+            function (yearChart) {
+                yearChart.selectAll('g.x text')
+                         .attr('dx', '-30')
+                         .attr('transform', 'rotate(-65)');
+            }
+        );
+        
+        raceCasesChart
+        .width(null)
+        .height(usChart.height() + usChart.width() / 100)
+        .ordering(function(d){ return d.value.avg})
+        .margins({ top: 10, left: 40, right: 40, bottom: 80})
+        .dimension(eduDim)
+        .group(percentByRace).valueAccessor(function(d) {
+            if (d.key != 1)
+            return Math.round(d.value.avg * 1000) / 1000;
+        })
+        .x(d3.scaleOrdinal().domain(["Multirace NH", "Black NH", "White NH", "Hispanic", "Other NH"]))
+        .xUnits(dc.units.ordinal)
+        .yAxisLabel("Average Asthma Percentage")
+        .xAxisLabel("Race")
+        .elasticY(true)
+        .transitionDuration(0)
+        .gap(10)
+        .renderlet(
+            function (yearChart) {
+                yearChart.selectAll('g.x text')
+                         .attr('dx', '-30')
+                         .attr('transform', 'rotate(-65)');
+                });
+
+        eduCasesChart
+        .width(null)
+        .height(usChart.height() + usChart.width() / 100)
+        .ordering(function(d){ return -d.value.avg})
+        .margins({ top: 10, left: 40, right: 40, bottom: 85})
+        .dimension(eduDim)
+        .group(percentByEdu).valueAccessor(function(d) {
+            if (d.key != 1)
+            return Math.round(d.value.avg * 1000) / 1000;
+        })
+        .x(d3.scaleOrdinal().domain(["HS Nongrad", "HS Grad", "Some Coll", "Coll Grad"]))
+        .xUnits(dc.units.ordinal)
+        .yAxisLabel("Average Asthma Percentage")
+        .xAxisLabel("Education")
+        .elasticY(true)
+        .transitionDuration(0)
+        .gap(10)
+        .renderlet(
+            function (yearChart) {
+                yearChart.selectAll('g.x text')
+                         .attr('dx', '-30')
+                         .attr('transform', 'rotate(-65)');
+                });
+
+        yearCasesChart
+        .ordering(function(d){ return -d.value.avg})
+        .cap(10)
+        .margins({ top: 10, left: 5, right: 80, bottom: 50})
+        .othersGrouper(false)
+        .width(null)
+        .height(usChart.height() + usChart.width() / 100)
+        .dimension(stateNameDim)
+        .group(percentByState).valueAccessor(function(d) {
+            return Math.round(d.value.avg * 1000) / 1000;
+        })
+        .transitionDuration(0)
+        .elasticX(true)
+        .xAxis().ticks(6);
+
+        yearChart
+        .width(null)
+        .height(usChart.height() + usChart.width() / 100)
+        .ordering(function(d){ return -d.value.avg})
+        .margins({ top: 10, left: 40, right: 40, bottom: 90})
+        .dimension(incomeDim)
+        .group(percentByIncome).valueAccessor(function(d) {
+            if (d.key != 1)
+            return Math.round(d.value.avg * 1000) / 1000;
+        })
+        .x(d3.scaleOrdinal().domain(["< $15,000", "$15-$24,999", "$25-$49,999", "$50-$74,999", ">=$75,000"]))
+        .xUnits(dc.units.ordinal)
+        .yAxisLabel("Average Asthma Percentage")
+        .xAxisLabel("Income")
+        .elasticY(true)
+        .transitionDuration(0)
+        .gap(10)
+        .renderlet(
+            function (yearChart) {
+                yearChart.selectAll('g.rect')
+                        .attr('fill', 'red');
+                yearChart.selectAll('g.x text')
+                         .attr('dx', '-30')
+                         .attr('transform', 'rotate(-65)');
+            }
+        );
+
+        yearPercentChart
+        .width(null)
+        .height(usChart.height() + usChart.width() / 100)
+        .margins({ top: 10, left: 30, right: 30, bottom: 50})
+        .x(d3.scaleLinear().domain([2011, 2024]))
+        //.y(d3.scaleLinear().domain([6, 13]))
+        .yAxisLabel("Percent of Population with Asthma")
+        .xAxisLabel("Year")
+        .elasticY(true)
+        .elasticX(true)
+        .yAxisPadding(0.5)
+        .xAxisPadding(0.5)
+        .dimension(yearDim)
+        .group(percentByYear)
+        .valueAccessor(function(d) {
+            return Math.round(d.value.avg * 1000) / 1000;
+        })
+        .transitionDuration(0)
+        .renderHorizontalGridLines(true)
+        .round(d3.format("d"))
+        .brushOn(true)
+        .renderlet(
+            function (yearChart) {
+                yearChart.selectAll('g.x text')
+                         .attr('dx', '-20')
+                         .attr('transform', 'rotate(-65)')
+            }
+        );
+
+        var xAxis = yearPercentChart.xAxis().tickFormat(d3.format('d'));
+
+        usChart.width(null)
         .transitionDuration(0)
         .height(function() {
             if (window.innerWidth < 768) {
@@ -890,7 +1233,73 @@ d3.json("/asthma/projects").then(function(data) {
                         + "\n"
                         + "Asthma Percentage: " + (p["value"]) + "%";
             });
-                dc.renderAll();
+
+            dc.renderAll();
+
+            function AddXAxis(chartToUpdate, displayText)
+            {
+                chartToUpdate.svg()
+                    .append("text")
+                    .attr("class", "x-axis-label")
+                    .attr("text-anchor", "middle")
+                    .attr("x", chartToUpdate.width()/2)
+                    .attr("y", chartToUpdate.height() - 4)
+                    .text(displayText);
+            }
+            // AddXAxis(raceCasesChart, "Race");
+            // AddXAxis(incomeCasesChart, "Income");
+
+            function average_percent(d) {
+                var sum = 0;
+                d.forEach(function(k, v) {
+                    sum += v;
+                });
+                return d.size() ? sum / d.size() : 0;
+            }
+
+            function nonzero_min(chart) {
+                dc.override(chart, 'yAxisMin', function () {
+                     var min = d3.min(chart.data(), function (layer) {
+                         return d3.min(layer.values, function (p) {
+                             return p.y + p.y0;
+                         });
+                     });
+                     return dc.utils.subtract(min, chart.yAxisPadding());
+                });
+                return chart;
+            }
+
+            function remove_empty_bins(source_group) {
+                return {
+                    all:function () {
+                        return source_group.all().filter(function(d) {
+                            return d.value != 0;
+                        });
+                    }
+                };
+            }
+
+            function find_max_year(data) {
+                var maxDate = new Date("2010-03-31 00:00:00");
+                console.log(data.Year);
+                data.forEach(function(d) {
+                    if (d.Year > maxDate) {
+                        maxDate = d.Year;
+                    }
+                })
+                return maxDate;
+            }
+
+            function find_min_year(data) {
+                var minDate = new Date("2023-03-31 00:00:00");
+                console.log(data);
+                data.forEach(function(d) {
+                    if (d.Year > maxDate) {
+                        minDate = d.Year;
+                    }
+                })
+                return minDate;
+            }
             })
 
     });
